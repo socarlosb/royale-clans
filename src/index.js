4eb4a148e035e4388f06f6d2726c461a1bfda6f5
Carlos <socarlosb@gmail.com>
2020-03-31 21:31:14 +0100

    added rate limit of 5sec

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 
 const { getOneClanData, getOnePlayerData } = require("./controllers");
+
+const limiter = rateLimit({
+  windowMs: 5000,
+  max: 1,
+  message: "too many requests, try again in a 5 seconds"
+});
 
 const App = express();
 
 App.use(cors());
 App.use(express.json());
+App.use(limiter);
 
 // '/api/clan/:tag' get clan info
 App.get("/api/clan/:tag", getOneClanData);
